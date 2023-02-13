55b00971a4f2bceec8c9132c20cc9a1e0580e39a
Kris <kris.bieliaeva@gmail.com>
2023-02-14 00:18:45 +0300

    added slider, modal window

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 // cart counter
 
 let productsCount = document.getElementById("productsCount");
-console.log(productsCount);
-
 let addToCartBtns = document.querySelectorAll(".add-to-cart");
-console.log(addToCartBtns);
-
-let count = 0;
 
 for (let i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener("click", function () {
-    productsCount.textContent = ++count;
+    productsCount.textContent = +productsCount.textContent + 1;
   });
 }
 
@@ -19,8 +14,47 @@ for (let i = 0; i < addToCartBtns.length; i++) {
 let like = document.querySelectorAll(".like-icon");
 console.log(like);
 
-like.forEach(function (like) {
-  like.addEventListener("click", function () {
-    like.classList.toggle("like-icon-clicked");
+like.forEach((item) => {
+  item.addEventListener("click", function () {
+    item.classList.toggle("like-icon-clicked");
   });
+});
+
+// modal window more details
+
+let moreDetailsBtn = document.querySelectorAll(".details");
+let modalWindow = document.querySelector(".modal");
+let closeBtn = document.querySelector(".btn-close");
+
+function openModal() {
+  modalWindow.classList.add("show");
+}
+
+function closeModal() {
+  modalWindow.classList.remove("show");
+}
+
+moreDetailsBtn.forEach((item) => {
+  item.addEventListener("click", openModal);
+});
+
+// modal while scroll
+
+const pageHeight = document.body.scrollHeight;
+const halfPage = pageHeight / 2;
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= halfPage) {
+    openModal();
+  }
+});
+
+closeBtn.addEventListener("click", closeModal);
+
+// slider
+
+$(".slider").slick({
+  dots: true,
+  arrows: true,
+  // autoplay: true,
+  // autoplaySpeed: 1000,
 });
